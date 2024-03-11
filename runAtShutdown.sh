322a974206bed5f1e265d68e63822131a128af6c
Matthew Austin <s2187553@ed.ac.uk>
2024-03-11 20:16:36 +0000

    pi push

---
+++ runAtShutdown.sh
@@ -0,0 +1,7 @@
+
+mount -oremount,rw /
+
+cd /home/pi/SDP-Group-4
+shutdown.sh
+
+mount -oremount,ro /
--- startup.sh
+++ startup.sh
@@ -1,4 +1,12 @@
+sleep 5
+
 cd /home/pi/SDP-Group-4
 git pull
 
-mkdir FUCK
+bash -c 'ifconfig -a > /home/pi/SDP-Group-4/NetworkLog.log 2>&1'
+
+git add --all
+
+git commit --all --message "pi boot"
+
+git push
