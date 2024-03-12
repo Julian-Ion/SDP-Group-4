38585ded6251a8d67428f9568f2e6a472f5ef6f8
Matthew Austin <s2187553@ed.ac.uk>
2024-03-12 17:23:34 +0000

    pi boot

--- startup.sh
+++ startup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+sleep 5
+
 while ! ping -c 4 google.com > /dev/null; 
 do 
 echo "The network is not up yet"
 sleep 1 
 done
 
+echo "Connected to network"
+
+sudo git config --global --add safe.directory /home/pi/SDP-Group-4
+
 cd /home/pi/SDP-Group-4
 git pull
+
+echo "Pull success"
 
 bash -c 'ifconfig -a > /home/pi/SDP-Group-4/NetworkLog.log 2>&1'
 
@@ -16,3 +24,5 @@ git add --all
 git commit --all --message "pi boot"
 
 git push
+
+echo "Push success"
