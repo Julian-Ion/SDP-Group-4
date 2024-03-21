3fa3cebecfa4aee17d1b0e23dad79f387c38440a
MattAu02 <s2187553@ed.ac.uk>
2024-03-21 18:19:46 +0000

    test

--- RaspberryPi/motor.py
+++ RaspberryPi/motor.py
@@ -11,6 +11,6 @@ run_time = 2 # number of seconds to run motors
 mc.move_motor(motor_id,speed) 
 start_time = time() # Encoder board can be fragile - always use a try/except loop 
 while time() < start_time + run_time: 
-    mc.print_encoder_data() 
+    #mc.print_encoder_data() 
     sleep(0.2) # Use a sleep of at least 0.1, to avoid errors 
 mc.stop_motors()
