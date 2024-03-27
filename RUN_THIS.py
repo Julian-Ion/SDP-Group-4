eacefc1b726396613774c6354884a40207af656c
Sam McCormack <samvjsmccormack@gmail.com>
2024-03-27 10:53:51 +0000

    sam

--- RUN_THIS.py
+++ RUN_THIS.py
@@ -11,7 +11,7 @@ class Controller:
     def __init__(self) -> None:
             
         self.JOYSTICK: SimpleJoystick = SimpleJoystick()
-        self.ARDUINO_INTERFACE: ArduinoInterface = ArduinoInterface()
+        # self.ARDUINO_INTERFACE: ArduinoInterface = ArduinoInterface()
         self.SYSTEM: str = platform.system()
 
         return None
@@ -40,19 +40,20 @@ class Controller:
             if output["square"]: # Turning
                 x_speed: float = self.transformRange(output["x"])
                 print(f"Square: Turning {x_speed}")
-                self.ARDUINO_INTERFACE.setTurnMode()
-                self.ARDUINO_INTERFACE.leftSpeed = x_speed
-                self.ARDUINO_INTERFACE.rightSpeed = -x_speed
-                self.ARDUINO_INTERFACE.update()
-            elif output["Triangle"]:
+                # self.ARDUINO_INTERFACE.setTurnMode()
+                # self.ARDUINO_INTERFACE.leftSpeed = x_speed
+                # self.ARDUINO_INTERFACE.rightSpeed = -x_speed
+                # self.ARDUINO_INTERFACE.update()
+            elif output["triangle"]:
                 print("Triangle: Set speed 0.7")
-                self.ARDUINO_INTERFACE.setMotorSpeed(0.7)
+                # self.ARDUINO_INTERFACE.setMotorSpeed(0.7)
             elif output["circle"]:
                 print("Circle: Set speed 1")
-                self.ARDUINO_INTERFACE.setMotorSpeed(1.0)
+                # self.ARDUINO_INTERFACE.setMotorSpeed(1.0)
             else:
-                print("No buttons: Manual Speed")
-                self.ARDUINO_INTERFACE.setMotorSpeed(self.transformRange(output["y"]))
+                y_speed: float = self.transformRange(output["y"])
+                print(f"No buttons: Manual Speed {y_speed}")
+                # self.ARDUINO_INTERFACE.setMotorSpeed(y_speed)
 
             sleep(0.1)
             self.clear()
